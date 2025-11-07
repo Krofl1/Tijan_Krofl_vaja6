5d9866b66a42b68742c4329ca84891ed9ec41c8d
Krofl1 <tijankrofl2008@gmail.com>
2025-11-07 09:22:20 +0100

    Dodani komentarji

--- Vaja6/Program.cs
+++ Vaja6/Program.cs
@@ -13,21 +13,21 @@ namespace vaja6 {
         public string u_ime;
         public string u_geslo;
         public static int stIg = 1;
-
+// lastnosti igralec
         public Igralec() {
             u_ime = "";
             u_geslo = "";
             id = stIg;
             stIg++;
         }
-
+// metoda za vraacanje idja
         public int Id {
             get
             {
                 return id;
             }
         }
-
+// metoda za vracanje in pokrasanje imena
         public string U_ime {
             get
             {
@@ -38,7 +38,7 @@ namespace vaja6 {
                 u_ime = value.Trim();
             }
         }
-
+metoda za vreacanje in pokrajsanje gesla
         public string U_geslo
         {
             get
